1905258f23ff321e4745cd8d71321f719c670b16
Student <Student@et.qcc.edu>
2026-01-28 10:52:40 -0500

    start lab 3

---
+++ lab3/lab3_Chowdhury.cpp
@@ -0,0 +1,21 @@
+/*
+Sabed Chowdhury
+Jan 28, 2026
+lab 3, numerical variables
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"\n------ Example 1: ------"<<endl;
+    // declare variables
+    int number = 10.9;
+    float n = 7.8;
+    
+    cout<<"number = "<<number<<endl;
+    cout<<"n = "<<n<<endl;
+
+    return 0;
+}
